4622f9a82c74f66d674a77eaa14240a85a4eb9a0
jdf18 <97624626+jdf18@users.noreply.github.com>
2024-09-17 20:55:18 +0100

    Fixed install script

--- link.ps1
+++ link.ps1
@@ -1,2 +1,2 @@
-New-Item C:\Users\jawad\.emacs.d\init.el       -ItemType SymbolicLink -Target "$(Get-Location)/init.el"
-New-Item C:\Users\jawad\.emacs.d\early-init.el -ItemType SymbolicLink -Target "$(Get-Location)/early-init.el"
+New-Item $env:USERPROFILE\.emacs.d\init.el       -ItemType SymbolicLink -Target "$(Get-Location)/init.el"
+New-Item $env:USERPROFILE\.emacs.d\early-init.el -ItemType SymbolicLink -Target "$(Get-Location)/early-init.el"
